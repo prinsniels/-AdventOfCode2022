ae199c40ce59a2e191bf72e2558445558d65d00a
niels prins <niels.prins@gmail.com>
2022-12-08 23:04:17 +0100

    simplified implementation

--- src/main/scala/day07.scala
+++ src/main/scala/day07.scala
@@ -1,57 +1,52 @@
 package aoc
 import scala.util.chaining.*
 
-object day07 extends App:
+object tmp extends App:
 
-  type Path = Vector[String]
+  enum StdOutLine:
+    case Cd(v: String)
+    case Dir(v: String)
+    case File(v: String, size: Int)
+    case Ls
 
-  trait Tree:
-    val name: String
-    val size: Int
+  object StdOutLine:
+    def apply(v: String): StdOutLine =
+      v match
+        case s"$$ cd $a" => Cd(a)
+        case s"$$ ls"    => Ls
+        case s"dir $a"   => Dir(a)
+        case s"$a $b"    => File(b, a.toInt)
 
-  case class Folder(name: String, children: List[Tree]) extends Tree:
-    val size = children.map(_.size).sum
+  enum DiskObject:
+    case File(name: String, size: Int)
+    case Dir(path: List[String])
 
-  case class File(name: String, size: Int) extends Tree
+  val diskStruct = "day07.txt".live
+    .map(x => StdOutLine(x))
+    .foldLeft((List.empty[String], List.empty[(DiskObject.Dir, DiskObject)])) {
+      case ((path, collection), out) =>
+        out match
+          case StdOutLine.Cd(v) if v == "/"  => (List("/"), collection)
+          case StdOutLine.Cd(v) if v == ".." => (path.tail, collection)
+          case StdOutLine.Cd(v)              => (v :: path, collection)
+          case StdOutLine.Dir(v)        => (path, (DiskObject.Dir(path), DiskObject.Dir(v :: path)) :: collection)
+          case StdOutLine.File(v, size) => (path, (DiskObject.Dir(path), DiskObject.File(v, size)) :: collection)
+          case StdOutLine.Ls            => (path, collection)
 
-  extension (fld: Folder)
-    def push(path: Path, f: File): Folder =
-      path match
-        case hd +: tail =>
-          fld.children.find(_.name == hd) match
-            case None => fld.copy(children = Folder(hd, List.empty).push(tail, f) :: fld.children)
-            case Some(v: Folder) =>
-              fld.copy(children = v.push(tail, f) :: fld.children.filterNot(_.name == hd))
-            case Some(_) => throw NotImplementedError()
-        case _ => fld.copy(children = f :: fld.children)
-
-    def filterSum(pred: Int => Boolean): Int =
-      (if pred(fld.size) then fld.size else 0) + fld.children
-        .collect { case f: Folder => f }
-        .map(_.filterSum(pred))
-        .sum
-
-    def doGet(pred: Int => Boolean): List[Folder] =
-      if pred(fld.size) then
-        fld :: fld.children.collect { case f: Folder => f }.flatMap(_.doGet(pred))
-      else fld.children.collect { case f: Folder => f }.flatMap(_.doGet(pred))
-
-  val fPattern = raw"(\d+) (\D+)".r
-  val root     = Folder("", List.empty)
-
-  lazy val fs = "day07.txt".live
-    .foldLeft(Vector.empty[String], root) { case ((path, root), line) =>
-      line match
-        case s"$$ cd $a" =>
-          if a == ".." then (path.dropRight(1), root)
-          else (path :+ a, root)
-        case fPattern(s, n) => (path, root.push(path, File(n, s.toInt)))
-        case _              => (path, root)
     }
     ._2
+    .groupBy(_._1)
+    .mapValues(x => x.map(_._2))
+
+  val size: DiskObject => Int = d =>
+    d match
+      case DiskObject.File(v, s) => s
+      case DiskObject.Dir(p)     => diskStruct.getOrElse(DiskObject.Dir(p), List.empty).map(size(_)).sum
+
+  val folderSizes: Map[DiskObject.Dir, Int] = diskStruct.keysIterator.map(k => k -> diskStruct(k).map(size).sum).toMap
 
   // Part 1.
-  fs.filterSum(_ <= 100000) pipe println
+  folderSizes.values.filter(_ <= 100_000).sum pipe println
 
   // Part 2.
-  fs.doGet(_ >= 30000000 - (70000000 - fs.size)).map(_.size).min pipe println
+  folderSizes.values.filter(_ >= 30000000 - (70000000 - (folderSizes(DiskObject.Dir(List("/")))))).min pipe println
