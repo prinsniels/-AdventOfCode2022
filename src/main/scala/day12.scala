1793480c44edcba52407a3f998c627517da44337
niels prins <niels.prins@gmail.com>
2022-12-12 09:27:39 +0100

    better comments

--- src/main/scala/day12.scala
+++ src/main/scala/day12.scala
@@ -13,6 +13,9 @@ object day12 extends App:
 
   val scores = ('a' to 'z').zipWithIndex.toMap + ('S' -> 0) + ('E' -> 25)
 
+  // This builds a function that can be seen as a Graph. A graph is brought
+  // back to a function that takes a `A` and returns a List[A] (A => List[A])
+  // in our case Vec => List[Vec] representing the the locations we can goto from A
   def g(b: Board[Char], s: Map[Char, Int]): Vec => List[Vec] = v =>
     val curHeight = s(b(v))
     List(up, down, left, right)
@@ -23,12 +26,14 @@ object day12 extends App:
   val start = board.find((v, c) => c == 'S').map((v, _) => v).get
   val end   = board.find((v, c) => c == 'E').map((v, _) => v).get
 
+  // utility function for searching the graph
   val search: Vec => Map[Vec, Vec] = v => breathFirst(g(board, scores), v)(x => ()).toMap
 
   // Part 1.
   unwind(search(start), end).size pipe println
 
   // Part 2.
+  // Check the path for all `a`
   board
     .filter((v, c) => c == 'a')
     .map((a, _) => unwind(search(a), end).size)
